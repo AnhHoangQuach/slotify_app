1f074d9b75300c63d6e511c1de9e4c31d5045a6f
AnhHoangQuach <hoanganh36.work@gmail.com>
2020-08-24 00:50:50 +0700

    complete album page

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
-<?php
-include("includes/config.php");
+<?php include("includes/header.php"); ?>
 
-//session_destroy(); LOGOUT
+	<h1 class="pageHeadingBig">You Might Also Like</h1>
+	<div class="gridViewContainer">
+		<?php 
+			$albumQuery = mysqli_query($con, "SELECT * FROM albums");
+			while($row = mysqli_fetch_array($albumQuery)) {
+				
 
-if(isset($_SESSION['userLoggedIn'])) {
-	$userLoggedIn = $_SESSION['userLoggedIn'];
-}
-else {
-	header("Location: register.php");
-}
+				echo "<div class='gridViewItem'>
+						<a href='album.php?id=" . $row['id'] . "'>
+							<img src='" . $row['artworkPath'] . "'>
+							<div class='gridViewInfo'>
+								" . $row['title'] . "
+							</div>
+						</a>
+					</div>";
+			}
+		?>
+	</div>
 
-?>
-
-<html>
-<head>
-	<title>Welcome to Slotify!</title>
-</head>
-
-<body>
-	Hello!
-</body>
-
-</html>
+<?php include("includes/footer.php"); ?>
